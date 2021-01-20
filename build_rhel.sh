b683c521342645c506ffd4f815cd56cb6acc9320
Massimiliano Pinto <massimiliano.pinto@gmail.com>
2021-01-20 13:18:25 +0100

    Use default gcc for building notification server
    Use default gcc for building notification server

--- build_rhel.sh
+++ build_rhel.sh
@@ -6,7 +6,7 @@
 
 # The new gcc 7 is now available using the command 'source scl_source enable devtoolset-7'
 # the previous gcc will be enabled again after this script exits
-source scl_source enable devtoolset-7
+# source scl_source enable devtoolset-7
 
 mkdir build
 cd build/
