d440f87e2035dbef7e2200134e0db27bfe947768
Mohit Tomar <m.singh19994@gmail.com>
2022-12-06 18:39:36 +0530

    Replace Check for Centos 7 to Centos Stream 9
    Signed-off-by: Mohit Tomar <m.singh19994@gmail.com>

--- build_rhel.sh
+++ build_rhel.sh
@@ -6,7 +6,8 @@
 
 # The new gcc 7 is now available using the command 'source scl_source enable devtoolset-7'
 # the previous gcc will be enabled again after this script exits
-source scl_source enable devtoolset-7
+# Commented scl_source  because it does not work in centos stream 9
+# source scl_source enable devtoolset-7
 
 mkdir build
 cd build/
