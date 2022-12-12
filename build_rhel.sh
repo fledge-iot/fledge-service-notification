092400d5c803e2c640e3887dbcbdd957f1b49693
Mohit Tomar <m.singh19994@gmail.com>
2022-12-12 13:20:52 +0530

    Removed OS NAME check from build_rhel.sh
    Signed-off-by: Mohit Tomar <m.singh19994@gmail.com>

--- build_rhel.sh
+++ build_rhel.sh
@@ -6,10 +6,9 @@
 
 # The new gcc 7 is now available using the command 'source scl_source enable devtoolset-7'
 # the previous gcc will be enabled again after this script exits
-os_name=$(grep -o '^NAME=.*' /etc/os-release | cut -f2 -d\" | sed 's/"//g')
 os_version=$(grep -o '^VERSION_ID=.*' /etc/os-release | cut -f2 -d\" | sed 's/"//g')
 # Use scl_source only if OS is RedHat/CentOS 7
-if [[ ( $os_name == *"Red Hat"* || $os_name == *"CentOS"* ) &&  $os_version == *"7"* ]]
+if [[ $os_version == *"7"* ]]
 then
     source scl_source enable devtoolset-7
 fi
