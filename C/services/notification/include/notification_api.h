cd4b4b2b539ef54f41af7f810c579a636041218d
nandan <nandan.ghildiyal@gmail.com>
2023-06-06 18:52:49 +0530

    FOGL-7697:  Updated regular expression for RECEIVE_NOTIFICATION
    Signed-off-by: nandan <nandan.ghildiyal@gmail.com>

--- C/services/notification/include/notification_api.h
+++ C/services/notification/include/notification_api.h
@@ -20,7 +20,7 @@ using HttpServer = SimpleWeb::Server<SimpleWeb::HTTP>;
  * URL for each API entry point
  */
 #define ESCAPE_SPECIAL_CHARS		"\\{\\}\\\"\\(\\)\\!\\[\\]\\^\\$\\.\\|\\?\\*\\+\\-"
-#define RECEIVE_NOTIFICATION		"^/notification/reading/asset/([A-Za-z][a-zA-Z0-9_%\\-\\.]*)$"
+#define RECEIVE_NOTIFICATION		"^/notification/reading/asset/([A-Za-z0-9][a-zA-Z0-9_%\\-\\.]*)$"
 #define RECEIVE_AUDIT_NOTIFICATION	"^/notification/reading/audit/([A-Za-z][a-zA-Z0-9_%\\-\\.]*)$"
 #define RECEIVE_STATS_NOTIFICATION	"^/notification/reading/stat/([A-Za-z][a-zA-Z0-9_%\\-\\.]*)$"
 #define RECEIVE_STATS_RATE_NOTIFICATION	"^/notification/reading/rate/([A-Za-z][a-zA-Z0-9_%\\-\\.]*)$"
