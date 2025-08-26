a272ca8f970e17d6b0417716190a4b11be070c2d
nandan <nandan.ghildiyal@gmail.com>
2025-08-26 13:11:08 +0530

    Fixed review comments
    Signed-off-by: nandan <nandan.ghildiyal@gmail.com>

--- C/services/notification/notification.cpp
+++ C/services/notification/notification.cpp
@@ -61,7 +61,7 @@ static void signalHandler(int signal)
 // Displays service information in JSON format
 static void printServiceInfoAsJSON()
 {
-	std::string serviceInfoJSON = R"({"name":"Notification Service","description":"Fledge Notification Service To Send Notifications","package":"fledge-service-notification","type":"notification","process_name":"notification_c","process_script":"[services/notification_c]"})" ;
+	static std::string serviceInfoJSON = R"({"name":"Notification Service","description":"Fledge Notification Service To Send Notifications","package":"fledge-service-notification","type":"notification","process_name":"notification_c","process_script":"[services/notification_c]"})" ;
 
 	std::cout << serviceInfoJSON << std::endl;
 }
